3af38cdc470ea76d34d2af3d88aea0065a466004
Todin13 <thomasodin13@gmail.com>
2023-12-16 20:33:45 +0100

    ex 1 without error

--- model.sql
+++ model.sql
@@ -3,19 +3,19 @@
 DO $$
     BEGIN
 
-        CREATE IF NOT EXIST TABLE stocks (
+        CREATE TABLE IF NOT EXISTS stocks (
             id serial PRIMARY KEY,
             stock_name varchar UNIQUE,
             stock_value int
         );
 
-        CREATE IF NOT EXIST TABLE stocks_historic (
-            stock_id varchar UNIQUE REFERENCES (stocks.id),
+        CREATE TABLE IF NOT EXISTS stocks_historic (
+            stock_id int REFERENCES stocks(id),
             stock_value int,
             complete_date timestamp NOT NULL
         );
 
-        CREATE IF NOT EXIST TABLE clients (
+        CREATE TABLE IF NOT EXISTS clients (
             id serial PRIMARY KEY,
             first_name varchar NOT NULL,
             last_name varchar NOT NULL,
@@ -23,19 +23,19 @@ DO $$
             phone_number varchar UNIQUE NOT NULL
         );
 
-        CREATE IF NOT EXIST TABLE wallet (
+        CREATE TABLE IF NOT EXISTS wallet (
             id serial PRIMARY KEY,
-            client_id int UNIQUE REFERENCES (clients.id) ON DELETE RESTRICT,
+            client_id int UNIQUE REFERENCES clients(id) ON DELETE RESTRICT,
             actions_number int DEFAULT(0)
         );
 
-        CREATE IF NOT EXIST TABLE options (
+        CREATE TABLE IF NOT EXISTS options (
             id serial PRIMARY KEY,
-            stock_id int REFERENCES (stocks.id) ON DELETE RESTRICT,
-            wallet_id int REFERENCES (wallet.id) ON DELETE RESTRICT,
-            option_type varchar CHECK (otpion_type IN ('call','put')),
+            stock_id int REFERENCES stocks(id) ON DELETE RESTRICT,
+            wallet_id int REFERENCES wallet(id) ON DELETE RESTRICT,
+            option_type varchar CHECK (option_type IN ('call','put')),
             strike_price int NOT NULL,
-            expiration timestamp DEFAULT(NOW + '1 day')
+            expiration timestamp DEFAULT(NOW() + '1 day')
         );
 
 END $$
