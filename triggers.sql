aa999ea1308d8c24f19790681230c78e9860f80f
Todin13 <thomasodin13@gmail.com>
2023-12-16 22:29:44 +0100

    update triggers and functions

--- triggers.sql
+++ triggers.sql
@@ -3,8 +3,47 @@
 DO $$
     BEGIN
         
-        rajoute 1 au nombre d'action d'un wallet on insert de actions 
-        on insert de client create the wallet
-        on insert / update  de stock rejoute la nouvelle valeur du stock dans historique
+        CREATE OR REPLACE FUNCTION update_wallet_actions()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            UPDATE wallet
+            SET actions_number = options_number + 1
+            WHERE id = NEW.wallet_id;
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER update_wallet_actions_trigger
+        AFTER INSERT ON options
+        FOR EACH ROW
+        EXECUTE FUNCTION update_wallet_actions();
+
+        CREATE OR REPLACE FUNCTION create_wallet_for_new_client()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            INSERT INTO wallet (client_id)
+            VALUES (NEW.id);
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER create_wallet_trigger
+        AFTER INSERT ON clients
+        FOR EACH ROW
+        EXECUTE FUNCTION create_wallet_for_new_client();
+
+        CREATE OR REPLACE FUNCTION update_stock_historic()
+        RETURNS TRIGGER AS $$
+        BEGIN
+            INSERT INTO stocks_historic (stock_id, stock_value, complete_date)
+            VALUES (NEW.id, NEW.stock_value, NEW.complete_date);
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER update_stock_historic_trigger
+        AFTER INSERT OR UPDATE ON stocks
+        FOR EACH ROW
+        EXECUTE FUNCTION update_stock_historic();
 
 END $$ 
