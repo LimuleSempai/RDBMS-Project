c6ff816b747d4719f4e67fe1106cc2836f900098
Todin13 <thomasodin13@gmail.com>
2023-12-17 15:31:28 +0100

    brownian_motion

--- monte_carlo.sql
+++ monte_carlo.sql
@@ -1,1 +1,16 @@
+CREATE OR REPLACE PROCEDURE brownian_motion(
+    num_simulations int, 
+    num_steps int, 
+    dt numeric,
+    OUT result double precision[]
+)
+AS $$
+DECLARE
+    wt double precision[];
+BEGIN
+    wt := ARRAY(SELECT ARRAY(SELECT SQRT(dt) * random() FROM generate_series(1, num_simulations)) FROM generate_series(1, num_steps));
+    result := wt;
+END;
+$$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE PROCEDURE monte_carlo_simulation()    
